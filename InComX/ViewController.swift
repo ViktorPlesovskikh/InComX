af9278c97144509c328f00d46ea10067ef6609c8
Viktor Plesovskikh <viktor.p2015@icloud.com>
2022-06-20 21:31:03 +1100

    Lesson 1

--- InComX/ViewController.swift
+++ InComX/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBAction func LogInButton(_ sender: Any) {
+    }
+    
+    @IBOutlet var EmailTextField: UITextField!
+    
+    @IBOutlet var PassTextField: UITextField!
+    @IBAction func RegisterNowButton(_ sender: Any) {
+    }
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
