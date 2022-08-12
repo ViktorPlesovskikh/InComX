aa1b8224c952c2073f661d0d4a0e8261dc28f4d9
Viktor Plesovskikh <viktor.p2015@icloud.com>
2022-08-12 23:23:01 +1100

    + LoginViewController

---
+++ InComX/Controller/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  InComX
+//
+//  Created by Виктор Плесовских on 12.08.2022.
+//
+
+import UIKit
+import Alamofire
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+  
+
+}
